043c9afe02d432076612ccd08ccd9eb3ccea2634
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-29 15:28:04 +0400

    feat: add / path

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Index from '@/pages/Index';
+
 Vue.use(VueRouter)
 
   const routes = [
+    {
+      path: '/',
+      component: Index
+    }
 ]
 
 const router = new VueRouter({
