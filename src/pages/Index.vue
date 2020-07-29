da855c2e427e86df0b4a35caeb61fdbb4fd59a58
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-29 15:28:27 +0400

    feat: add index page

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page__index">
+    <AppForm />
+  </div>
+</template>
+
+<script>
+import AppForm from "@/components/Form/AppForm";
+
+export default {
+  components: {
+    AppForm,
+  },
+};
+</script>
+
+<style>
+  .page__index {
+  }
+</style>
