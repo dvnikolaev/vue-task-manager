e3a2806d906045077ad892a570562cfceb865e1e
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-30 14:40:00 +0400

    feat: add methods completeTask and rejectTask

--- src/components/Tasks/ItemTask.vue
+++ src/components/Tasks/ItemTask.vue
@@ -1,13 +1,13 @@
 <template>
   <v-list-item>
     <v-list-item-content>
-      <v-list-item-title>{{ task.title }}</v-list-item-title>
+      <v-list-item-title>{{ task.title }} {{ task.id }}</v-list-item-title>
       <v-list-item-subtitle>{{ task.description }}</v-list-item-subtitle>
       <v-list-item-subtitle>{{ task.date }}</v-list-item-subtitle>
     </v-list-item-content>
     <v-list-item-action class="flex-row" v-if="!task.completed">
-      <v-btn success color="success" class="mr-4">Завершить</v-btn>
-      <v-btn outlined color="error">Отклонить</v-btn>
+      <v-btn success color="success" class="mr-4" @click="completeTask(task.id)">Завершить</v-btn>
+      <v-btn outlined color="error" @click="rejectTask(task.id)">Отклонить</v-btn>
     </v-list-item-action>
   </v-list-item>
 </template>
@@ -16,6 +16,14 @@
 export default {
   props: {
     task: Object
+  },
+  methods: {
+    completeTask(id) {
+      this.$store.commit('completeTask', id);
+    },
+    rejectTask(id) {
+      this.$store.commit('rejectTask', id);
+    }
   }
 }
 </script>
