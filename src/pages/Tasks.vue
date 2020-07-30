cedd8954ab2db4d8a0eaff8cbd07101e77236eaa
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-30 13:18:53 +0400

    feat: add taskInWork

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -1,11 +1,16 @@
 <template>
-  <CardTasks />
+  <CardTasks :tasks="tasksInWork" />
 </template>
 
 <script>
 import CardTasks from "@/components/Tasks/CardTasks";
 
 export default {
+  computed: {
+    tasksInWork() {
+      return this.$store.getters.getInWorkTasks;
+    },
+  },
   components: {
     CardTasks,
   },
