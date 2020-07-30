b56ec99485d777dee4d260e58f9ddd0d70d48183
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-30 16:10:58 +0400

    style: change flex sm

--- src/components/Tasks/ItemTask.vue
+++ src/components/Tasks/ItemTask.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-list-item>
+  <v-list-item class="flex-column flex-sm-row">
     <v-list-item-content>
       <v-list-item-title>{{ task.title }}</v-list-item-title>
       <v-list-item-subtitle>{{ task.description }}</v-list-item-subtitle>
       <v-list-item-subtitle>{{ task.date }}</v-list-item-subtitle>
     </v-list-item-content>
-    <v-list-item-action class="flex-row" v-if="!task.completed">
-      <v-btn success color="success" class="mr-4" @click="completeTask(task.id)">Завершить</v-btn>
-      <v-btn outlined color="error" @click="rejectTask(task.id)">Отклонить</v-btn>
+    <v-list-item-action class="align-self-end flex-row justify-sm-start" v-if="!task.completed">
+      <v-btn success  color="success" class="mr-4" @click="completeTask(task.id)">Завершить</v-btn>
+      <v-btn outlined  color="error" @click="rejectTask(task.id)">Отклонить</v-btn>
     </v-list-item-action>
   </v-list-item>
 </template>
