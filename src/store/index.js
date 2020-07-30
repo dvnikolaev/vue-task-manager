49c46297b2f8a162902e4b7e420cb40cc7eaa8ed
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-30 13:12:25 +0400

    change task model

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,66 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    tasks: []
+    tasks: [
+      {
+        title: "Content filtering",
+        description:
+          "Set the content filtering level to restrict appts that can be downloaded",
+        date: "2020-08-08",
+        completed: false
+      },
+      {
+        title: "Content filtering",
+        description:
+          "Set the content filtering level to restrict appts that can be downloaded",
+        date: "2020-08-08",
+        completed: false
+      },
+      {
+        title: "Content filtering",
+        description:
+          "Set the content filtering level to restrict appts that can be downloaded",
+        date: "2020-08-08",
+        completed: false
+      },
+      {
+        title: "Content filtering",
+        description:
+          "Set the content filtering level to restrict appts that can be downloaded",
+        date: "2020-08-08",
+        completed: true
+      },
+      {
+        title: "Content filtering",
+        description:
+          "Set the content filtering level to restrict appts that can be downloaded",
+        date: "2020-08-08",
+        completed: true
+      },
+    ],
   },
   mutations: {
     addTask(store, payload) {
-     store.tasks.push({
-      title: payload.title,
-      description: payload.description,
-      date: payload.date,
-      status: payload.status
-     });
-    }
-  },
-  actions: {
-  },
-  getters: {
-    getInWorkTasks: state => {
-      return state.tasks.filter(item => {
-        item.status === 'inwork';
-      })
-    },
-    getCompletedTasks: state => {
-      return state.tasks.filter(item => {
-        item.status === 'completed'
+      store.tasks.push({
+        title: payload.title,
+        description: payload.description,
+        date: payload.date,
+        completed: false,
       });
-    }
+    },
   },
-  modules: {
-  }
-})
+  actions: {},
+  getters: {
+    getInWorkTasks: (state) => {
+      return state.tasks.filter((item) => !item.completed);
+    },
+    getCompletedTasks: (state) => {
+      return state.tasks.filter((item) => item.completed);
+    },
+  },
+  modules: {},
+});
