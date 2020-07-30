9ac09d6fc0b88c86fa0544924cfd23f7ef7be071
dvnikolaev <dvnikolaeev@gmail.com>
2020-07-30 13:26:11 +0400

    feat: add props task

--- src/components/Tasks/ItemTask.vue
+++ src/components/Tasks/ItemTask.vue
@@ -1,13 +1,21 @@
 <template>
   <v-list-item>
     <v-list-item-content>
-      <v-list-item-title>Content filtering</v-list-item-title>
-      <v-list-item-subtitle>Set the content filtering level to restrict appts that can be downloaded</v-list-item-subtitle>
-      <v-list-item-subtitle>2020-09-09</v-list-item-subtitle>
+      <v-list-item-title>{{ task.title }}</v-list-item-title>
+      <v-list-item-subtitle>{{ task.description }}</v-list-item-subtitle>
+      <v-list-item-subtitle>{{ task.date }}</v-list-item-subtitle>
     </v-list-item-content>
-    <v-list-item-action class="flex-row">
+    <v-list-item-action class="flex-row" v-if="!task.completed">
       <v-btn success color="success" class="mr-4">Завершить</v-btn>
       <v-btn outlined color="error">Отклонить</v-btn>
     </v-list-item-action>
   </v-list-item>
 </template>
+
+<script>
+export default {
+  props: {
+    task: Object
+  }
+}
+</script>
